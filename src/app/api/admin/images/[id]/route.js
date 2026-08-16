200516c8f9d3a4a68380c8cc67327b109d80cb30
artinasd <128320019+artinasd@users.noreply.github.com>
2026-08-17 00:40:42 +0330

    Add admin product image delete API

---
+++ src/app/api/admin/images/[id]/route.js
@@ -0,0 +1,7 @@
+import { deleteProductImage } from '../../../../../../server/admin/images.js';
+import { apiErrorResponse, apiSuccess } from '../../../../../../server/api/response.js';
+
+export async function DELETE(_request, { params }) {
+  try { return apiSuccess({ image: await deleteProductImage(Number(params.id)) }); }
+  catch (error) { return apiErrorResponse(error, 'Unable to delete product image.'); }
+}
